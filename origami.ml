7bd20dd5e2e81195dec5af70bf7849a5670049f4
Jaskier234 <jaskier234@gmail.com>
2018-12-11 15:35:34 +0100

    Add files via upload

---
+++ origami.ml
@@ -0,0 +1,39 @@
+
+type point = float * float
+
+type kartka = point -> int
+
+let prostokat (p1:point) (p2:point) = fun (a:point) ->
+  match a, p1, p2 with
+  | (a,b), (x1,y1), (x2,y2) ->
+    if x1 <= a && a <= x2 && y1 <= b && b <= y2 then 1 else 0
+
+let sq x = x*.x
+
+let kolko (p:point) r = fun (a:point) ->
+  match a, p with
+  | (a,b), (x1,y1) ->
+    if sq(x1-.a) +. sq(y1-.b) <= sq r then 1 else 0
+
+let na_lewo (bx, by) (cx, cy) (ax, ay) =
+  (cx-.bx)*.(ay-.by) -. (ax-.bx)*.(cy-.by)
+
+let sym (p1x, p1y) (p2x, p2y) (cx,cy) =
+  let (cwx, cwy) = (cx -. p1x, cy -. p1y) in
+  let (pwx, pwy) = (p2x -. p1x, p2y -. p1y) in
+  let dot = (cwx*.pwx) +. (cwy*.pwy) in
+  let pmagsq = (pwx*.pwx) +. (pwy*.pwy) in
+  let (ox, oy) = (pwx /. pmagsq *. dot +. p1x, pwy /. pmagsq *. dot +. p1y) in
+  (cx +. 2.0*.(ox -. cx), cy +. 2.0*.(oy -. cy))
+
+let zloz (p1:point) (p2:point) (k:kartka) = fun (x,y) -> 
+  let l = na_lewo p1 p2 (x,y) in
+  if l = 0. then k (x,y) 
+  else if l < 0. then 0
+  else k (x,y) + k (sym p1 p2 (x,y))
+
+let skladaj lista kartka =
+  let f k (p1,p2) = zloz p1 p2 k in
+  List.fold_left f kartka lista
+    
+  
